0ee6d3416e0c026a2176e3adea1afac098cb9dbe
廿一日的船 <aprosail@outlook.com>
2024-07-28 17:39:40 +0800

    整理目录避免过度嵌套

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from "vitepress"
+import {DefaultTheme, defineConfig} from "vitepress"
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -18,32 +18,44 @@ export default defineConfig({
     ],
 
     sidebar: {
-      "/about/": {
-        base: "/about/",
-        items: [{text: "平台建设初衷", link: "about.md"}],
-      },
-      "/code/": {
-        base: "/code/",
-        items: [{text: "软件开发", link: "code.md"}],
-      },
-      "/market/": {
-        base: "/market/",
-        items: [{text: "经济财会", link: "market.md"}],
-      },
-      "/mech/": {
-        base: "/mech/",
-        items: [{text: "机械设计", link: "mech.md"}],
-      },
-      "/math/": {
-        base: "/math/",
-        items: [{text: "数学", link: "math.md"}],
-      },
-      "/nature/": {
-        base: "/nature/",
-        items: [{text: "基础科学", link: "nature.md"}],
-      },
+      "/about/": {base: "/about/", items: sidebarAbout()},
+      "/code/": {base: "/code/", items: sidebarCode()},
+      "/market/": {base: "/market/", items: sidebarMarket()},
+      "/mech/": {base: "/mech/", items: sidebarMech()},
+      "/math/": {base: "/math/", items: sidebarMath()},
+      "/nature/": {base: "/nature/", items: sidebarNature()},
     },
 
     socialLinks: [{icon: "github", link: "https://github.com/vuejs/vitepress"}],
   },
 })
+
+/** 初衷、声明和基本使用说明。 */
+function sidebarAbout(): DefaultTheme.SidebarItem[] {
+  return [{text: "平台建设初衷", link: "about.md"}]
+}
+
+/** 编程基础及跨平台软件开发。 */
+function sidebarCode(): DefaultTheme.SidebarItem[] {
+  return [{text: "软件开发", link: "code.md"}]
+}
+
+/** 经济和经济法、财务、会计。 */
+function sidebarMarket(): DefaultTheme.SidebarItem[] {
+  return [{text: "经济财会", link: "market.md"}]
+}
+
+/** 机械设计、机电设计和嵌入式控制。 */
+function sidebarMech(): DefaultTheme.SidebarItem[] {
+  return [{text: "机械设计", link: "mech.md"}]
+}
+
+/** 作为基础知识的工程数学，包含初等数学和高等数学。 */
+function sidebarMath(): DefaultTheme.SidebarItem[] {
+  return [{text: "工程数学", link: "math.md"}]
+}
+
+/** 基础科学知识。 */
+function sidebarNature(): DefaultTheme.SidebarItem[] {
+  return [{text: "基础科学", link: "nature.md"}]
+}
