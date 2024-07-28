35130100b61678152936c80c7d13f20cb5149278
廿一日的船 <aprosail@outlook.com>
2024-07-28 17:23:22 +0800

    修复资源相对路径引用

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from "vitepress"
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "./",
   title: "数社知识工程",
   description: "技能互助平台",
   themeConfig: {
